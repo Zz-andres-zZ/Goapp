4a7acd3e03813853a032f43206972894b2bc9f3e
Andres H <andresjhmndz@gmail.com>
2023-02-08 19:48:52 -0400

    ""

--- view/login.php
+++ view/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign up</title>
+    <title>Sign in</title>
     <link rel="shortcut icon" href="../asset/images/user.png" type="image/x-icon">
     <link rel="stylesheet" href="../asset/css/form.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
@@ -13,7 +13,7 @@
 
 <body>
 
-    
+
 
 
 
